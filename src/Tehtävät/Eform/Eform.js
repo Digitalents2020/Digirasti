b4ead58d789f5601175835fe8d531d5dbc1ee8aa
Noora Peltokangas <noora.peltok@gmail.com>
2021-08-25 12:28:03 +0300

    E-form and email exercises now finished

--- src/Tehtävät/Eform/Eform.js
+++ src/Tehtävät/Eform/Eform.js
@@ -4,6 +4,8 @@ import "./Eform.css";
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}> {text} </button>
 );
+
+/*This state change part was done by following the mooc.fi fullstack tutorial. But the gist of it is that we have an array that aps a "p" (for pressed) in to the array*/
 const Email = () => {
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
@@ -13,6 +15,7 @@ const Email = () => {
     setRight(right + 1);
   };
 
+/*Here we check wether the array is empty and we remain on this state, or if there is an item, we switch state to the next one*/  
   const History = (props) => {
     if (props.allClicks.length === 0) {
       return (
@@ -22,6 +25,7 @@ const Email = () => {
           </p>
           <p>Ei huolta, tässäkään tehtävässä ei oikeasti lähetetä mitään minnekkään</p>
           <h5>Hakulomake</h5>
+          <br></br>
           <div className="field">
             <form>
               <label>Etunimi</label>
@@ -37,19 +41,28 @@ const Email = () => {
               <label>Työkokemus 2</label>
               <textarea></textarea>
               <label>Korkein koulutusaste</label>
-              <select>
+              <select className='degree'>
+                  <option>Valitse koulutusaste...</option>
                   <option>Kansakoulu</option>
                   <option>Peruskoulu</option>
                   <option>Ammattikoulu</option>
-                  <option>Kaksoistutkinto</option>
+                  <option>Opisto</option>
                   <option>Ylioppilastutkinto</option>
+                  <option>Alempi korkeakoulututkinto</option>
+                  <option>Ylempi korkeakoulututkinto</option>
+                  <option>Ei mikään näistä (Selitä alempana tarkemmin)</option>
               </select>
+              <br></br>
+              <label>Tutkintonimike</label>
+              <input type="text"></input>
+              <br></br>
               <label>Kerro kolmella lauseella, millainen työntekijä olisit</label>
               <textarea></textarea>
               <label>Lataa liite</label>
               <input type="file"></input>
               <Button handleClick={handleRightClick} text="Lähetä" />
             </form>
+            <br></br>
           </div>
         </div>
       );
