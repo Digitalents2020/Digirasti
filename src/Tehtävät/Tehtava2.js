2c50a57cc231f49450995e885a627e1818012771
lipsjas <86725104+lipsjas@users.noreply.github.com>
2021-09-24 17:55:05 +0300

    Updated logic for tehtava1 and saving changes for tehtava2
    Tehtava1 is the main commit here. Tehtava2 has some unifying edits yet it is not done yet. I want to save the changes to version control anyway.

--- src/Tehtävät/Tehtava2.js
+++ src/Tehtävät/Tehtava2.js
@@ -1,83 +1,64 @@
 import React, { useState } from 'react'
 
-const Button = ( props ) => {
-  return (
-    <button onClick={ props.handleClick }>
-    { props.label }
-    </button>
-  )
-}
-
 const Tehtava2 = () => {
-  const [demoMessage, setDemoMessage] = useState('')
+  const [ counter, setCounter ] = useState( 0 )
+  const [ taskText, setTaskText ] = useState('')
 
-    const textareastyle = {
-        minHeight: '220px',
-		    minWidth: '500px'
+  const textareastyle = {
+      minHeight: '100px',
+      minWidth: '95%'
+  }
+
+  setTimeout(() => {
+    laskurifunktio()
+  }, 1000)
+
+  const laskurifunktio = () => {
+    setCounter( counter + 1 )
+  }
+
+  const textChangeEvent = ( event ) => {
+    alert( `Hienoa, olet tehnyt tämän tehtävän onnistuneesti!\nVoit nyt jatkaa seuraavaan tehtävään.\n\nSulkeaksesi tämän ilmoituksen, paina OK-painiketta.` )
+    console.log( "Tekstikenttä triggered" )
+    setTaskText( event.target.value )
+  }
+  
+  const Sivukomponentti1 = () => { 
+    return (
+      <div>
+        <p><br/></p>
+        <p><b>Ohje:</b>Kirjoita seuraava teksti alla olevaan kenttään. Teksti antaa sinulle vihjeitä, miten voit toimia, kun kirjoitat tekstiä.</p>
+        <p><br/></p>
+      </div>
+      )
     }
     
-    const Sivukomponentti1 = () => { 
-        return (
-          <div>
-            <p><br/></p>
-            <p><b>Ohje:</b>Kirjoita seuraava teksti alla olevaan kenttään. Teksti antaa sinulle vihjeitä, miten voit toimia, kun kirjoitat tekstiä.</p>
-            <p><br/></p>
-          </div>
-        )
-      }
-      
-      const alertDemo = () => {
-        alert( "Tällainen on oletus alert-ikkuna. Ruma mutta yleinen." )
-      }
-      const notificationDemo = () => {
-        if( demoMessage === '' )
-        setDemoMessage( "Tämä on erilainen ilmoitusikkuna." )
-        else {
-          setDemoMessage( '' )
-        }
-      }
+    const Sivukomponentti2 = () => { 
+      return (
+        <div>
+          <p><br/></p>
+          <p>Shift-näppäin on näppäimistön vasemmalla laidalla oleva näppäin, jossa on nuoli ylöspäin. Erikoismerkit ja isot kirjaimet saa painamalla Shiftiä ja kirjainta yhtä aikaa. Kun kirjoitat pitkää tekstiä tekstinkäsittelyohjelmalla, sinun ei tarvitse vaihtaa riviä painamalla Enteriä, vaan voit jatkaa kirjoittamista, vaikka rivin pääty lähestyy. Kun haluat vaihtaa kappaletta, voit painaa Enteriä, jolloin kappaleiden väliin tulee hieman tyhjää tilaa.</p>
+          <p>Tekstinkäsittelyohjelma hoitaa rivinvaihdon puolestasi. Saat tehtyä huutomerkin painamalla Shift-näppäintä ja numeroa 1 samaan aikaan!</p>
+          <p><br/></p>
+        </div>
+      )
+    }
 
-      const Notification = ({ message }) => {
-        if (message === '') {
-          return null
-        }
-        return (
-          <div className="demoNotification">
-            {message}
-          </div>
-        )
-      }
-
-      const Sivukomponentti2 = () => { 
-          return (
-            <div>
-              <textarea style={textareastyle}></textarea>
-              <p><br/></p>
-            </div>
-          )
-        }
-
-        const Sivukomponentti3 = () => { 
-            return (
-              <div>
-                <Button handleClick={alertDemo} label='Alert'/>
-                <Button handleClick={notificationDemo} label='Ilmoitusikkuna'/>
-                <p><br/></p>
-                <Notification message={demoMessage}/>
-                <p><br/></p>
-                <p>Shift-näppäin on näppäimistön vasemmalla laidalla oleva näppäin, jossa on nuoli ylöspäin. Erikoismerkit ja isot kirjaimet saa painamalla Shiftiä ja kirjainta yhtä aikaa. Kun kirjoitat pitkää tekstiä tekstinkäsittelyohjelmalla, sinun ei tarvitse vaihtaa riviä painamalla Enteriä, vaan voit jatkaa kirjoittamista, vaikka rivin pääty lähestyy. Kun haluat vaihtaa kappaletta, voit painaa Enteriä, jolloin kappaleiden väliin tulee hieman tyhjää tilaa.</p>
-                <p>Tekstinkäsittelyohjelma hoitaa rivinvaihdon puolestasi. Saat tehtyä huutomerkin painamalla Shift-näppäintä ja numeroa 1 samaan aikaan!</p>
-                <p><br/></p>
-              </div>
-            )
-          }
+    const Sivukomponentti3 = () => { 
+      return (
+        <div>
+          <textarea style={textareastyle} onChange={textChangeEvent} value={taskText}></textarea>
+          <p><br/></p>
+        </div>
+      )
+    }
 
     return (
         <div>
             <h1>Tehtävä 2</h1>
             <Sivukomponentti1/>
-            <Sivukomponentti3/>
             <Sivukomponentti2/>
+            <Sivukomponentti3/>
         </div>
     )
 }
